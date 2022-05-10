56b1149f9aebb7192690727f7b450be473095e06
蘇健豪 <jenhausu@osensetech.com>
2022-05-10 14:12:41 +0800

    feat: arquicklook

--- ARQuickLook-UIKIt/ViewController.swift
+++ ARQuickLook-UIKIt/ViewController.swift
@@ -6,14 +6,32 @@
 //
 
 import UIKit
+import QuickLook
+import ARKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        let previewController = QLPreviewController()
+        previewController.dataSource = self
+        present(previewController, animated: true, completion: nil)
+    }
 
 }
 
+extension ViewController: QLPreviewControllerDataSource {
+    func numberOfPreviewItems(in controller: QLPreviewController) -> Int {
+        1
+    }
+    
+    func previewController(_ controller: QLPreviewController, previewItemAt index: Int) -> QLPreviewItem {
+        guard let path = Bundle.main.path(forResource: "yuketen", ofType: "usdz") else { fatalError("Couldn't find the supported input file.") }
+        let url = URL(fileURLWithPath: path)
+        return url as QLPreviewItem
+    }
+}
